05b9daca0f1847dd0a069cc18cd003952296e247
MDan9382 <160198160+MDan9382@users.noreply.github.com>
2024-02-23 22:04:06 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int dn;
+Console.WriteLine("ingrese el dia de la semana en numeros (1-7)");
+dn = Convert.ToInt16(Console.ReadLine());
+switch (dn)
+{
+    case 1:
+        Console.WriteLine("hoy es lunes");
+        break;
+    case 2:
+        Console.WriteLine("hoy es martes");
+        break;
+    case 3:
+        Console.WriteLine("hoy es miercoles");
+        break;
+    case 4:
+        Console.WriteLine("hoy es jueves");
+        break;
+    case 5:
+        Console.WriteLine("hoy es viernes");
+        break;
+    case 6:
+        Console.WriteLine("hoy es sabado");
+        break;
+    case 7:
+        Console.WriteLine("hoy es domingo");
+        break;
+    default:
+        Console.WriteLine("COMANDO INVALIDO");
+        break;
+}
---
+++ parte 2,3.cs
@@ -0,0 +1,19 @@
+﻿string s;
+Console.WriteLine("ingrese el servicio que desea pagar");
+Console.WriteLine(" 1)lavado de auto\n2)revision mecanica\n3)cambio de aceite");
+s = Console.ReadLine().ToUpper();
+switch (s)
+{
+    case "1" or "LAVADO DE AUTO":
+        Console.WriteLine("el importe a pagar es de Q40");
+        break;
+    case "2" or "REVISION MECANICA":
+        Console.WriteLine("el importe a pagar es de Q200");
+        break;
+    case "3" or "CAMBIO DE ACEITE":
+        Console.WriteLine("el importe a pagar es de Q100");
+        break; //no se cuanto valen estas cosas... no tengo carro :(
+    default: 
+        Console.WriteLine("COMANDO INVALIDO");
+        break;
+}
